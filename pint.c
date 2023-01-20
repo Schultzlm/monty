56a9792123e4d0baabbeda3f9f2ace1cabae3647
schultzlm <litokwas@programmer.net>
2023-01-19 18:50:43 -0800

    pint

--- pint.c
+++ pint.c
@@ -2,14 +2,14 @@
 /**
  * f_pint - prints the top
  * @head: the stack head
- * @counter: the line-number
+ * @number: the line-number
  * Return: no return
  */
-void f_pint(stack_t **head, unsigned int counter)
+void f_pint(stack_t **head, unsigned int number)
 {
 	if (*head == NULL)
 	{
-		fprintf(stderr, "L%u: can't print, stack empty\n", counter);
+		fprintf(stderr, "L%u: can't print, stack empty\n", number);
 		fclose(bus.file);
 		free(bus.content);
 		free_stack(*head);
